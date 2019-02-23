171e0609cbbcbeb0f212981e76453fc156d419c6
toilari <ilari97@hotmail.com>
2019-02-23 20:29:58 +0200

    Send whole user on login

--- controllers/login.js
+++ controllers/login.js
@@ -26,7 +26,7 @@ loginRouter.post('/', async (request, response) => {
 
   return response
     .status(200)
-    .send({ token, username: user.username, name: user.name })
+    .send({ token, ...user.toJSON() })
 })
 
 module.exports = loginRouter
